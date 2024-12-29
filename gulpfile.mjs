8871298692245b0524f0de92c8dbed59f3fbc4e4
Prototype <prototype444@hotmail.com>
2024-12-29 11:32:34 -0800

    quick error fixes

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -18,26 +18,26 @@ const MODULE_SOURCE_PATH = "./module";
 export async function link() {
     const args = process.argv.splice(3, process.argv.length - 3);
     if (args.length !== 2 || args[0] !== "--user-data-path") {
-        Logger.error("ERROR: Missing --user-data-path Argument");
+        console.error("ERROR: Missing --user-data-path Argument");
         return;
     }
 
     const dataPath = args[1];
     if (!fs.existsSync(dataPath)){
-        Logger.error("ERROR: User data path not found");
+        console.error("ERROR: User data path not found");
         return;
     }
 
     const linkFile = `${dataPath}/Data/modules/${PACKAGE_ID}`;
     if (fs.existsSync(linkFile)){
-        Logger.error("ERROR: module directory already exists. Uninstall module and try again.");
+        console.error("ERROR: module directory already exists. Uninstall module and try again.");
         return;
     }
 
     fs.symlink(path.resolve(MODULE_SOURCE_PATH), linkFile, "dir", (err) => {
         if (err) 
-            Logger.error("ERROR: failed to link");
+            console.error("ERROR: failed to link");
         else
-            Logger.info(`Linked ${path.resolve(MODULE_SOURCE_PATH)} folder to ${linkFile}.`);
+            console.info(`Linked ${path.resolve(MODULE_SOURCE_PATH)} folder to ${linkFile}.`);
     });
 }
--- module/scripts/settings.mjs
+++ module/scripts/settings.mjs
@@ -1,5 +1,5 @@
 // a function that registers all the settings used by the module
-export function registerSettings() {
+export default function registerSettings() {
     game.settings.register("shadowdark-crawl-helper", "lastVersion", {
 		name: "shadowdark-crawl-helper.lastVersion",
 		default: "",
